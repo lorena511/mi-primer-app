8e693e252838d9643daaa4b282ddc6b002aaf06e
Lorena <160654986+lorena511@users.noreply.github.com>
2024-06-26 23:09:05 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <h2>Hello Dojo!</h2>
+        <h3>Things I need to do:
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Maraton</li>
+          <li>Feed the dogs</li>
+        </h3>
+    </>
+  )
+}
+
+export default App
